fe589068841e41763bd3dba8398b601515f02806
tmarts <tmarts@student.42heilbronn.de>
2024-05-11 19:55:02 +0200

    mariadb made more secure

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Check if the WordPress database directory does not exit & start the MariaDB server directly, run in backgroudn (&)
+# Check if the WordPress database directory does not exist & start the MariaDB server directly, run in backgroudn (&)
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
     mysqld --user=mysql &
 
-    # Wait for MariaDB daemon to start
-    sleep 5
+	# Wait for MariaDB daemon to start
+	until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD_FILE} &>/dev/null; do
+   		echo "Waiting for MariaDB to start..."
+    	sleep 1
+	done
 
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "CREATE DATABASE ${MYSQL_DATABASE};"
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_FILE}';"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_FILE}';"
 	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "FLUSH PRIVILEGES;"
 	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD_FILE}';"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "FLUSH PRIVILEGES;"
 
 else
-   mysqld
+   mysqld --user=mysql
 fi
--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -3,7 +3,7 @@
 # <<Script is run as the user www-data>>
 
 # Wait for mariadb to start
-sleep 5
+# sleep 5
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	echo "Creating wordpress configuration file ..."
@@ -30,7 +30,7 @@ if ! wp user get $WP_USER >/dev/null; then
 	echo "Creating a new user ..."
     wp user create  $WP_USER $WP_USER_EMAIL --role=author --prompt=user_pass < $WP_USER_PASSWORD_FILE || { echo "Failed to create user" >&2; exit 1; }
 fi
-# --user_pass=$WP_USER_PASSWORD_FILE \
+
 echo "Starting php-fpm ..."
 php-fpm7.4 -F
 # -F flag means foreground
