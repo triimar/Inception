6681594e556f84b6bfc629e376d2fd49d26ff239
tmarts <tmarts@student.42heilbronn.de>
2024-05-12 20:10:04 +0200

    found problems on first startup, fixed

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -4,17 +4,18 @@
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
     mysqld --user=mysql &
 
-	# Wait for MariaDB daemon to start
-	until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD_FILE} &>/dev/null; do
-   		echo "Waiting for MariaDB to start..."
-    	sleep 1
-	done
+	# # Wait for MariaDB daemon to start
+	# until mysqladmin -u root -p${MYSQL_ROOT_PASSWORD_FILE} &>/dev/null; do
+   	# 	echo "Waiting for MariaDB to start..."
+    # 	sleep 1
+	# done
+	sleep 5
 
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_FILE}';"
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD_FILE}';"
-	mysql -u root -p${MYSQL_ROOT_PASSWORD_FILE} -e "FLUSH PRIVILEGES;"
+	mysql -u root -p"$(cat ${MYSQL_ROOT_PASSWORD_FILE})" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+	mysql -u root -p"$(cat ${MYSQL_ROOT_PASSWORD_FILE})" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat ${MYSQL_PASSWORD_FILE})';"
+	mysql -u root -p"$(cat ${MYSQL_ROOT_PASSWORD_FILE})" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+	mysql -u root -p"$(cat ${MYSQL_ROOT_PASSWORD_FILE})" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MYSQL_ROOT_PASSWORD_FILE})';"
+	mysql -u root -p"$(cat ${MYSQL_ROOT_PASSWORD_FILE})" -e "FLUSH PRIVILEGES;"
 
 else
    mysqld --user=mysql
--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -2,8 +2,37 @@
 
 # <<Script is run as the user www-data>>
 
-# Wait for mariadb to start
-# sleep 5
+#Wait for mariadb to start
+# sleep 20
+
+# # Check if WordPress is not installed
+# if ! wp core is-installed --path="/var/www/html" >/dev/null; then
+#     echo "Downloading and installing WordPress..."
+    
+#     # Download WordPress
+#     wp core download --path="/var/www/html" --locale=en_US || { echo "Failed to download WordPress" >&2; exit 1; }
+
+#     # Create WordPress configuration file
+#     wp config create --dbname=$WP_DB_NAME \
+#                      --dbuser=$WP_DB_USER \
+#                      --prompt=dbpass < $WP_DB_PASSWORD_FILE \
+#                      --dbhost="$WP_DB_HOST:$WP_DB_PORT" \
+#                      --dbprefix=$WP_TABLE_PREFIX || { echo "Failed to create WordPress configuration file" >&2; exit 1; }
+    
+#     # Install WordPress
+#     wp core install --url=localhost \
+#                     --title=Inception \
+#                     --admin_user=$WP_ADMIN_USER \
+#                     --prompt=admin_password < $WP_ADMIN_PASSWORD_FILE \
+#                     --admin_email=$WP_ADMIN_EMAIL || { echo "Failed to install WordPress" >&2; exit 1; }
+# else
+#     echo "WordPress is already installed."
+# fi
+
+if ! wp core is-installed >/dev/null 2>&1; then
+	wp core download --path=/var/www/html/ --locale=en_US \
+					|| { echo "Failed to download WordPress" >&2; exit 1; }				
+fi
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	echo "Creating wordpress configuration file ..."
@@ -12,23 +41,24 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
                 	--prompt=dbpass < $WP_DB_PASSWORD_FILE \
                 	--dbhost="$WP_DB_HOST:$WP_DB_PORT" \
 					--dbprefix=$WP_TABLE_PREFIX \
-					|| { echo "Failed to create WordPress configuration file" >&2; exit 1; }
+					--quiet || { echo "Failed to create WordPress configuration file" >&2; exit 1; }
 	chmod 644 wp-config.php || { echo "Failed to set permissions for wp-config.php" >&2; exit 1; }
 fi
 
-if ! wp core is-installed >/dev/null; then
-	echo "Downloading and installing wordpress ..."
-	wp core download --path=/var/www/html --locale=en_US
+if ! wp core is-installed >/dev/null 2>&1; then
+	echo "Installing wordpress ..."
 	wp core install --url=localhost \
 					--title=Inception \
 					--admin_user=$WP_ADMIN_USER \
 					--prompt=admin_password < $WP_ADMIN_PASSWORD_FILE \
-					--admin_email=$WP_ADMIN_EMAIL || { echo "Failed to install WordPress" >&2; exit 1; }				
+					--admin_email=$WP_ADMIN_EMAIL \
+					--quiet || { echo "Failed to install WordPress" >&2; exit 1; }				
 fi
 
-if ! wp user get $WP_USER >/dev/null; then
+if ! wp user get $WP_USER >/dev/null 2>&1; then
 	echo "Creating a new user ..."
-    wp user create  $WP_USER $WP_USER_EMAIL --role=author --prompt=user_pass < $WP_USER_PASSWORD_FILE || { echo "Failed to create user" >&2; exit 1; }
+    wp user create  $WP_USER $WP_USER_EMAIL --role=author --prompt=user_pass < $WP_USER_PASSWORD_FILE \
+					--quiet || { echo "Failed to create user" >&2; exit 1; }
 fi
 
 echo "Starting php-fpm ..."
